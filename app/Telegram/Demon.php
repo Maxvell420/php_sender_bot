ad38cca89fa709c9ade29fe588118c45b190ce66
Maxvell420 <maksimvelichko1998@mail.ru>
2025-09-04 21:32:01 +0300

    проставляю Id сообщениям

--- app/Telegram/Demon.php
+++ app/Telegram/Demon.php
@@ -3,7 +3,7 @@
 namespace App\Telegram;
 
 use App\Libs\Telegram\TelegramRequest;
-use App\Models\ {
+use App\Models\{
     Update,
     Job
 };
@@ -11,14 +11,16 @@ use App\Telegram\UseCases\Updates;
 use Exception;
 use App\Telegram\UseCases\JobsHandler;
 
-class Demon {
+class Demon
+{
 
-    public function run(): void {
+    public function run(): void
+    {
         $secret = env('TG_BOT_SECRET');
         $telegram = new TelegramRequest($secret);
 
         $useCase = new Updates($telegram);
-        while( true ) {
+        while (true) {
             $update = new Update();
             $update_id = $update->getNextUpdateId();
 
@@ -26,18 +28,17 @@ class Demon {
                 $updates = $telegram->getUpdates($update_id, 10);
                 print('приняты обновления' . "\n");
 
-                if( empty($updates['result']) ) {
+                if (empty($updates['result'])) {
                     print('обновлений нет ищу работы' . "\n");
                     $job = new Job();
                     $job = $job->findFirstNotCompleted();
 
-                    if( $job ) {
+                    if ($job) {
                         print("Выполняю работу $job->id" . "\n");
                         $jobHandler = new JobsHandler($telegram);
                         $jobHandler->handleJob($job);
                         print("Выполнил работу $job->id" . "\n");
-                    }
-                    else {
+                    } else {
                         print('работ нет' . "\n");
                     }
 
@@ -46,16 +47,15 @@ class Demon {
 
                 print('обновления есть' . "\n");
 
-                foreach($updates['result'] as $update) {
+                foreach ($updates['result'] as $update) {
                     $useCase->handleUpdate($update);
                 }
             } catch (Exception $e) {
                 $status = $e->getCode();
 
-                if( str_starts_with(4, "$status") ) {
+                if (str_starts_with(4, "$status")) {
                     $useCase->handleErrorUpdate($update);
-                }
-                else {
+                } else {
                     // Как-то обрабатывать, сейчас просто спать час
                     sleep(env('WRONG_ANSWER'));
                 }
--- app/Telegram/UseCases/JobsHandler.php
+++ app/Telegram/UseCases/JobsHandler.php
@@ -4,29 +4,32 @@ namespace App\Telegram\UseCases;
 
 use App\Libs\Telegram\TelegramActions;
 use App\Libs\Telegram\TelegramRequest;
-use App\Models\ {
+use App\Models\{
     Job,
     JobUser
 };
 use App\Telegram\Enums;
 
-class JobsHandler {
+class JobsHandler
+{
 
     public function __construct(private TelegramRequest $telegramRequest, private MessageBuilder $messageBuilder = new MessageBuilder) {}
 
-    public function handleJob(Job $job): void {
-        match($job->job_type) {
+    public function handleJob(Job $job): void
+    {
+        match ($job->job_type) {
             Enums\JobTypes::Create_post->value => $this->handleSendPost($job),
         };
     }
 
-    private function handleSendPost(Job $job): void {
+    private function handleSendPost(Job $job): void
+    {
         $count = 0;
 
         $update = json_decode($job->json, true);
 
-        foreach(TelegramActions::cases() as $case) {
-            if( $case->value == $update['action'] ) {
+        foreach (TelegramActions::cases() as $case) {
+            if ($case->value == $update['action']) {
                 $action = $case;
             }
         }
@@ -35,15 +38,17 @@ class JobsHandler {
         $userJobs = $userJobs->listByJob($job->id);
 
         /**
-            * @var JobUser[] $userJobs
-        */
+         * @var JobUser[] $userJobs
+         */
 
-        foreach($userJobs as $user) {
+        foreach ($userJobs as $user) {
             $count++;
 
-            if( $user->isCompleted() ) {
+            if ($user->isCompleted()) {
                 continue;
             }
+
+            $update['message']['chat_id'] = $user->actor_id;
 
             $this->telegramRequest->sendMessage($action, $update['message']);
             $user->complete();
