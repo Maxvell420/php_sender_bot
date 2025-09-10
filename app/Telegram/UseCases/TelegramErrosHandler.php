d50a3956e33132fcb9b96ab6df109f381ba57f8d
Maxvel <velichko.m@hub.sknt.ru>
2025-09-10 17:08:38 +0300

    начинаю делать обрабоку уже в юзкейсе

--- app/Telegram/UseCases/TelegramErrosHandler.php
+++ app/Telegram/UseCases/TelegramErrosHandler.php
@@ -3,11 +3,33 @@
 namespace App\Telegram\UseCases;
 
 use App\Libs\Telegram\TelegramActions;
+use App\Models\User;
 
 // Обработка различных ошибок
 class TelegramErrosHandler {
 
-    public function handleTelegramRequest(TelegramActions $action, array $data, int $status): void {}
+    public function handleTelegramRequest(TelegramActions $action, array $data, int $status): void {
+        if( $status == 403 && isset($data['chat_id']) ) {
+            $user = new User()->findByTgId($data['chat_id']);
+
+            if( $user ) {
+                $user->setKicked();
+                $user->save();
+            }
+
+            return;
+        }
+
+        match($action) {
+            TelegramActions::editMessageReplyMarkup => $this->handleSendReplyMarkUp($data, $status),
+            default => $this->handleSendData($data, $status)
+        };
+    }
+
+    private function handleSendReplyMarkUp(array $data, int $status): void {}
+    private function handleSendData(array $data, int $status): void {}
+
+    private function logError(): void {}
 
     public function handleWrongUpdate(array $data): void {}
 
