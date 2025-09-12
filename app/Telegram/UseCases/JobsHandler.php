610a037b731516f415fcdd31e8e7770fea71d3d3
Maxvell420 <maksimvelichko1998@mail.ru>
2025-09-12 05:09:46 +0300

    теперь рассылаю нормально

--- app/Telegram/UseCases/JobsHandler.php
+++ app/Telegram/UseCases/JobsHandler.php
@@ -2,24 +2,27 @@
 
 namespace App\Telegram\UseCases;
 
-use App\Models\ {
+use App\Models\{
     Job,
     JobUser
 };
 use App\Telegram\Enums;
 use App\Telegram\TelegramRequestFacade;
 
-class JobsHandler {
+class JobsHandler
+{
 
     public function __construct(private TelegramRequestFacade $telegramRequest, private MessageBuilder $messageBuilder) {}
 
-    public function handleJob(Job $job): void {
+    public function handleJob(Job $job): void
+    {
         match ($job->job_type) {
             Enums\JobTypes::Create_post->value => $this->handleSendPost($job),
         };
     }
 
-    private function handleSendPost(Job $job): void {
+    private function handleSendPost(Job $job): void
+    {
         $count = 0;
 
         $update = json_decode($job->json, true);
@@ -32,23 +35,23 @@ class JobsHandler {
 
         $actor_id = $job->actor_id;
 
-        foreach($userJobs as $user) {
-            if( $user->isCompleted() ) {
+        foreach ($userJobs as $user) {
+            if ($user->isCompleted()) {
                 continue;
             }
 
-            if( $user->actor_id == $actor_id ) {
+            if ($user->actor_id == $actor_id) {
                 continue;
             }
 
             $count++;
-            $update['message']['chat_id'] = $user->actor_id;
+            $message['chat_id'] = $user->actor_id;
             $this->telegramRequest->copyMessage($message);
             $user->complete();
             $user->save();
         }
 
-        $message = $this->messageBuilder->buildMessage(chat_id:$job->actor_id, text:"Пост был разослан $count пользователям");
+        $message = $this->messageBuilder->buildMessage(chat_id: $job->actor_id, text: "Пост был разослан $count пользователям");
         $this->telegramRequest->sendMessage($message);
         $job->complete();
         $job->save();
