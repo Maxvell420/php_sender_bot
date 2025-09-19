f77018e8839a88e030e8768fc67a4e9db32864fc
Maxvell420 <maksimvelichko1998@mail.ru>
2025-09-19 07:34:50 +0300

    теперь все работы

--- app/Telegram/UseCases/JobsHandler.php
+++ app/Telegram/UseCases/JobsHandler.php
@@ -3,24 +3,27 @@
 namespace App\Telegram\UseCases;
 
 use App\Libs\Telegram\TelegramActions;
-use App\Models\ {
+use App\Models\{
     Job,
     JobUser
 };
 use App\Telegram\Enums;
 use App\Telegram\TelegramRequestFacade;
 
-class JobsHandler {
+class JobsHandler
+{
 
     public function __construct(private TelegramRequestFacade $telegramRequest, private MessageBuilder $messageBuilder) {}
 
-    public function handleJob(Job $job): void {
+    public function handleJob(Job $job): void
+    {
         match ($job->job_type) {
             Enums\JobTypes::Create_post->value => $this->handleSendPost($job),
         };
     }
 
-    private function handleSendPost(Job $job): void {
+    private function handleSendPost(Job $job): void
+    {
         $count = 0;
 
         $update = json_decode($job->json, true);
@@ -34,27 +37,32 @@ class JobsHandler {
 
         $actor_id = $job->actor_id;
 
-        foreach($userJobs as $user) {
-            if( $user->isCompleted() ) {
+        foreach ($userJobs as $user) {
+            if ($user->isCompleted()) {
                 continue;
             }
 
-            if( $user->actor_id == $actor_id ) {
+            if ($user->actor_id == $actor_id) {
                 continue;
             }
 
             $count++;
             $message['chat_id'] = $user->actor_id;
-            match($action) {
+            match ($action) {
                 TelegramActions::copyMessage->value => $this->telegramRequest->copyMessage($message),
-                TelegramActions::copyMessages->value => $this->telegramRequest->copyMessages($message)
+                TelegramActions::copyMessages->value => $this->telegramRequest->copyMessages($message),
+                TelegramActions::sendMessage->value => $this->telegramRequest->sendMessage($message),
+                TelegramActions::sendDocument->value => $this->telegramRequest->sendDocument($message),
+                TelegramActions::sendPhoto->value => $this->telegramRequest->sendPhoto($message),
+                TelegramActions::sendVideo->value => $this->telegramRequest->sendVideo($message),
+                TelegramActions::sendAnimation->value => $this->telegramRequest->sendAnimation($message),
             };
 
             $user->complete();
             $user->save();
         }
 
-        $message = $this->messageBuilder->buildMessage(chat_id:$job->actor_id, text:"Пост был разослан $count пользователям");
+        $message = $this->messageBuilder->buildMessage(chat_id: $job->actor_id, text: "Пост был разослан $count пользователям");
         $this->telegramRequest->sendMessage($message);
         $job->complete();
         $job->save();
