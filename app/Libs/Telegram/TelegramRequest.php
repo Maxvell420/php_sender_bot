af62114b86e38406e3048a12ffdfcda66b9eeb4e
Maxvell420 <maksimvelichko1998@mail.ru>
2025-10-01 05:20:06 +0300

    исключение теперь передаю нормально

--- app/Libs/Telegram/TelegramRequest.php
+++ app/Libs/Telegram/TelegramRequest.php
@@ -5,7 +5,8 @@ namespace App\Libs\Telegram;
 use Error;
 use Exception;
 
-class TelegramRequest {
+class TelegramRequest
+{
 
     private string $tg_url = 'https://api.telegram.org';
 
@@ -15,14 +16,16 @@ class TelegramRequest {
      * Функция получения обновлений из telegram
      * @param int $offset на самом деле это update_id
      */
-    public function getUpdates(?int $offset = null, ?int $timeout = null): array {
+    public function getUpdates(?int $offset = null, ?int $timeout = null): array
+    {
         $url = $this->buildUrlFromAction(TelegramActions::getUpdates, $offset, $timeout);
         // Это параметры и переделать
         $data = ['timeout' => $timeout];
-        return $this->sendRequest($url, data:$data);
+        return $this->sendRequest($url, data: $data);
     }
 
-    public function sendMessage(TelegramActions $action, array $message, array $params = []): void {
+    public function sendMessage(TelegramActions $action, array $message, array $params = []): void
+    {
         $url = $this->tg_url;
         $data = ['post_fields' => $message, 'method' => CURLOPT_POST];
         $url = $this->buildUrlFromAction($action);
@@ -30,15 +33,16 @@ class TelegramRequest {
         $this->sendRequest($url, $data, $params);
     }
 
-    private function buildUrlFromAction(TelegramActions $action, ?int $offset = null, ?int $timeout = null): string {
+    private function buildUrlFromAction(TelegramActions $action, ?int $offset = null, ?int $timeout = null): string
+    {
         // тут что-то навертел и тоже нужно по другому формировать. через массив параметров?
         $query = '';
 
-        if( !$timeout ) {
+        if (!$timeout) {
             $timeout = 0;
         }
 
-        if( $offset ) {
+        if ($offset) {
             $query = '?' . http_build_query(
                 [
                     'offset' => $offset,
@@ -54,18 +58,19 @@ class TelegramRequest {
         return "$tg_url/bot$secret/$action->value$query";
     }
 
-    private function sendRequest(string $url, array $data, array $params = []): array {
+    private function sendRequest(string $url, array $data, array $params = []): array
+    {
         $timeout = $data['timeout'] ?? 10 + 1;
         $method = $data['method'] ?? CURLOPT_HTTPGET;
         $curl = curl_init($url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-        if( $method == CURLOPT_POST ) {
+        if ($method == CURLOPT_POST) {
             curl_setopt($curl, $method, true);
             $post_fiels = $data['post_fields'];
             curl_setopt($curl, CURLOPT_POSTFIELDS, $post_fiels);
 
-            if( isset($params['file']) ) {
+            if (isset($params['file'])) {
                 curl_setopt(
                     $curl,
                     CURLOPT_HTTPHEADER,
@@ -82,17 +87,14 @@ class TelegramRequest {
         $httpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
         $decoded_response = json_decode($response, true);
 
-        if( $httpCode == 200 ) {
+        if ($httpCode == 200) {
             return $decoded_response;
-        }
-        elseif( str_starts_with($httpCode, 4) ) {
+        } elseif (str_starts_with($httpCode, 4)) {
             throw new TelegramApiException($decoded_response['description'], $httpCode);
-        }
-        elseif( is_bool($response) ) {
-            throw new Exception('Ошибка с сетью?', $httpCode);
-        }
-        else {
-            throw new Error('Ошибка из тг');
+        } elseif (is_bool($response)) {
+            throw new Exception($response, $httpCode);
+        } else {
+            throw new Error($response);
         }
     }
 }
