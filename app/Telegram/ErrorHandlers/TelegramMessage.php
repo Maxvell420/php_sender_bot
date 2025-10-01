35abdadfd7c748eec0b7bf04eb644248daf21ca3
Maxvell420 <maksimvelichko1998@mail.ru>
2025-10-01 21:34:47 +0300

    правильно блочу юзеров

--- app/Telegram/ErrorHandlers/TelegramMessage.php
+++ app/Telegram/ErrorHandlers/TelegramMessage.php
@@ -2,32 +2,34 @@
 
 namespace App\Telegram\ErrorHandlers;
 
-use App\Models\ {
+use App\Models\{
     Log,
 };
-use App\Repositories\ {
+use App\Repositories\{
     LogRepository,
     UserRepository
 };
 
-use App\Telegram\ {
+use App\Telegram\{
     Enums,
 };
-use App\Telegram\Updates\ {
+use App\Telegram\Updates\{
     Update,
     CallbackQueryUpdate,
     MessageUpdate,
     MyChatMemberUpdate
 };
 
-class TelegramMessage {
+class TelegramMessage
+{
 
     public function __construct(private UserRepository $userRepository, private LogRepository $logRepository) {}
 
     private const string BLOCKED = 'Forbidden: bot was blocked by the user';
     private const string WRONG_REPLY_MARKUP = 'Bad Request: message is not modified: specified new message content and reply markup are exactly the same as a current content and reply markup of the message';
 
-    public function handleWrongUpdate(Update $update, string $error_message, int $status): void {
+    public function handleWrongUpdate(Update $update, string $error_message, int $status): void
+    {
         match ($update->getType()) {
             Enums\UpdateType::MyChatMember => $this->handleNewChatMember($update, $status, $error_message),
             Enums\UpdateType::CallbackQuery => $this->handleCallbackQuery($update, $status, $error_message),
@@ -36,24 +38,27 @@ class TelegramMessage {
         };
     }
 
-    public function handleWrongGetUpdates(string $message): void {
+    public function handleWrongGetUpdates(string $message): void
+    {
         $save_data = ['message' => $message, 'data' => 'no data'];
         $data = json_encode($save_data);
         $this->logError($data);
     }
 
     // Сейчас это просто блокировка юзера
-    public function handleWrongJob(int $user_id, string $text): void {
-        if( $text == self::BLOCKED ) {
+    public function handleWrongJob(int $user_id, string $text): void
+    {
+        if ($text == self::BLOCKED) {
             $this->blockUser($user_id);
         }
     }
 
-    private function handleMessage(MessageUpdate $update, int $status, string $error_message): void {
-        if( $error_message == self::BLOCKED ) {
+    private function handleMessage(MessageUpdate $update, int $status, string $error_message): void
+    {
+        if ($error_message == self::BLOCKED) {
             $user_id = $update->getUserId();
             $user = $this->userRepository->findByTgId($user_id);
-            $user->blockUser();
+            $user->setKicked();
             $this->userRepository->persist($user);
             return;
         }
@@ -61,17 +66,19 @@ class TelegramMessage {
         $this->handleSendData($update, $error_message);
     }
 
-    private function blockUser(int $user_id): void {
+    private function blockUser(int $user_id): void
+    {
         $user = $this->userRepository->findByTgId($user_id);
 
-        if( $user ) {
+        if ($user) {
             $user->setKicked();
             $this->userRepository->persist($user);
         }
     }
 
-    private function handleNewChatMember(MyChatMemberUpdate $update, int $status, string $error_message): void {
-        if( $status == 403 ) {
+    private function handleNewChatMember(MyChatMemberUpdate $update, int $status, string $error_message): void
+    {
+        if ($status == 403) {
             $user_id = $update->getUserId();
             $this->blockUser($user_id);
             return;
@@ -80,21 +87,24 @@ class TelegramMessage {
         $this->handleSendData($update, $error_message);
     }
 
-    private function handleCallbackQuery(CallbackQueryUpdate $update, int $status, string $error_message): void {
-        if( $error_message == self::WRONG_REPLY_MARKUP && $status == 400 ) {
+    private function handleCallbackQuery(CallbackQueryUpdate $update, int $status, string $error_message): void
+    {
+        if ($error_message == self::WRONG_REPLY_MARKUP && $status == 400) {
             return;
         }
 
         $this->handleSendData($update, $error_message);
     }
 
-    private function handleSendData(Update $update, string $error_message): void {
+    private function handleSendData(Update $update, string $error_message): void
+    {
         $save_data = ['message' => $error_message, 'data' => $update];
         $data = json_encode($save_data);
         $this->logError($data);
     }
 
-    private function logError(string $message): void {
+    private function logError(string $message): void
+    {
         $log = new Log();
         $log->info = $message;
         $this->logRepository->persist($log);
