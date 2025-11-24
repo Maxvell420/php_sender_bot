29880a60c051bdf00780debb2eccdb890934c2fa
Maxvel <velichko.m@hub.sknt.ru>
2025-11-24 10:23:59 +0300

    засыпает бот каждые 30 минут на 10 секунд чтобы разорвать соединение

--- app/Telegram/TelegramSenderDemon.php
+++ app/Telegram/TelegramSenderDemon.php
@@ -6,30 +6,33 @@ use App\Libs\Infra\Context;
 use App\Libs\Infra\InnerDemon;
 use App\Telegram\UseCases\Updates;
 
-class TelegramSenderDemon extends InnerDemon
-{
+class TelegramSenderDemon extends InnerDemon {
 
+    private int $connections = 0;
     private Updates $updates;
 
-    public function __construct(protected Context $cntx)
-    {
+    public function __construct(protected Context $cntx) {
         parent::__construct($cntx);
         $facade = new TelegramUpdatesFacade($this->cntx);
         $this->updates = $facade->getUpdatesCommander();
     }
 
-    protected function run(): void
-    {
+    protected function run(): void {
+        // каждые 40хчисло таймаута от телеги засыпает на 10 секунд чтобы разорвать соединение с телегой
+        if( $this->connections > 40 ) {
+            sleep(10);
+            $this->connections = 0;
+        }
+
         $this->updates->work();
+        $this->connections++;
     }
 
-    protected function handleException(string $message): void
-    {
+    protected function handleException(string $message): void {
         $this->updates->handleException($message);
     }
 
-    protected function handleError(string $message): void
-    {
+    protected function handleError(string $message): void {
         $this->updates->handleError($message);
     }
 }
